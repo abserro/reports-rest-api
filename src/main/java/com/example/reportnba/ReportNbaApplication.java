2ed6a7e460fafe18b8900f45ac2e723f254f64cd
absszerro <cryinghostt.56@gmail.com>
2023-11-10 16:10:58 +0300

    init commit

---
+++ src/main/java/com/example/reportnba/ReportNbaApplication.java
@@ -0,0 +1,13 @@
+package com.example.reportnba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportNbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportNbaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reportnba/ReportNbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reportnba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportNbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
